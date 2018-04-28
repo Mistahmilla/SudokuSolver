41a09c00e9cf46168c88649502a5bfce4d8ced5a
Matt <miller@cycop.org>
2018-04-27 20:18:50 -0400

    committing notessolver to develop

---
+++ src/main/java/org/mistahmilla/sudoku/solvers/NotesSolver.java
@@ -0,0 +1,50 @@
+package org.mistahmilla.sudoku.solvers;
+import org.mistahmilla.sudoku.board.Board;
+
+public class NotesSolver implements Solver{
+
+    private Board board;
+
+    public NotesSolver(Board board){
+        this.board = board;
+    }
+
+    public void solve(){
+        generatePossibleValues();
+        // TODO: 4/23/2018 add functionality for notes solver
+        checkBoard();
+    }
+
+    public void checkBoard(){
+        int changed;
+
+        do {
+            changed = 0;
+            for (int x = 0; x < 9; x++) {
+                for (int y = 0; y < 9; y++) {
+                    if (board.getSquare(x, y).getPossibleValues().size() == 1) {
+                        board.setValue(x, y, Integer.parseInt(board.getSquare(x, y).getPossibleValues().get(0).toString()));
+                        changed++;
+                    }
+                }
+            }
+        }while(changed>0);
+    }
+
+    void generatePossibleValues(){
+        for (int x= 0; x<9; x++){
+            for (int y = 0; y<9; y++){
+                if(!board.hasValue(x, y)){
+                    for(int i = 1; i<=9; i++) {
+                        if(board.validValue(x, y, i)){
+                            board.getSquare(x, y).addPossibleValue(i);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+
+}
